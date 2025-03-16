efae5858f0289f115c5bd4c5504b8ee5c42cd8e7
gusjengis <anthony.j.green@outlook.com>
2025-03-15 21:30:29 -0700

    fixed now() and added timestamp to date string function

--- src/time.rs
+++ src/time.rs
@@ -1,5 +1,14 @@
-use web_sys::window;
+use chrono::{Local, NaiveDateTime, TimeZone};
+use js_sys::Date;
 
 pub fn now() -> f64 {
-    window().unwrap().performance().unwrap().now()
+    Date::now()
+}
+
+pub fn format_timestamp(timestamp_ms: i64) -> String {
+    let datetime = NaiveDateTime::from_timestamp_millis(timestamp_ms).expect("Invalid timestamp");
+    let local_datetime = Local.from_utc_datetime(&datetime);
+
+    let formatted = local_datetime.format("%m-%d-%Y: %I:%M %P").to_string();
+    formatted.replace("am", "a").replace("pm", "p")
 }
